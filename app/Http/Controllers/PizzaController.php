381fcae17be7a05ad7077a823eb06a114d6b5cc9
Mohammad Shohel <mohammadshohel866@gmail.com>
2021-02-09 17:05:20 +0600

    Route setup, layout, PizzaController

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Pizza;
+
+class PizzaController extends Controller
+{
+
+  public function index() {
+
+    // $pizzas = Pizza::all();
+    // $pizzas = Pizza::orderBy('name', 'desc')->get();
+    // $pizzas = Pizza::where('type', 'hawaiian')->get();
+    $pizzas = Pizza::latest()->get();
+
+    return view('pizzas.index', [
+      'pizzas' => $pizzas,
+    ]);
+  }
+
+  public function show($id) {
+    // use the $id variable to query the db for a record
+    // $pizza = Pizza::find($id);
+    $pizza = Pizza::findOrFail($id);
+    return view('pizzas.show', ['pizza' => $pizza]);
+  }
+
+  public function create() {
+    return view('pizzas.create');
+  }
+
+}
---
+++ resources/views/pizzas/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="wrapper pizza-details">
+  <h1>Order for {{ $pizza->name }}</h1>
+  <p class="type">Type - {{ $pizza->type }}</p>
+  <p class="base">Base - {{ $pizza->base }}</p>
+  <p class="toppings">Extra toppings:</p>
+</div>
+<a href="/pizzas" class="back"> -- Back to all pizzas -- </a>
+@endsection
